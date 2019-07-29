d15c074ecd3faeac5460161b87f1c1581e59e7c1
Shaun <41623262+Shaunimsorry@users.noreply.github.com>
2019-07-29 13:47:58 +0800

    Create switchCaseTest.cs
    Testing Switch/Case

---
+++ switchCaseTest.cs
@@ -0,0 +1,12 @@
+    public void loadlevel()
+    {
+        switch(selectedObject.name)
+        {
+            case "baselineTest_shapeChange":
+                selectedLevel = 2;
+                break;
+            case "baselineTest_blinker":
+                selectedLevel = 3;
+                break;
+        }
+    }
